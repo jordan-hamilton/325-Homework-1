cffd2dbb0ff0f8429f6fe92701f804f46d4077d8
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-06-30 15:56:38 -0600

    Prompt for file name

---
+++ mergesort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+
+int main() {
+
+  ifstream inputFile;
+  ofstream outputFile;
+  string inFileName, outFileName;
+
+  do {
+    std::cout << "Please enter the name of a file to read: " << endl;
+    getline(cin, inFileName);
+    inputFile.open(inFileName);
+  } while (!inputFile);
+
+}
